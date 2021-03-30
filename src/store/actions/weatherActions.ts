3dc414c4e081e6b27c540e392931137a89f27fb1
NeroN-95 <romaburlaka1995@gmail.com>
2021-03-30 21:31:20 +0300

    Creating WeatherMap with Type Script / redux-thunk / redux

--- src/store/actions/weatherActions.ts
+++ src/store/actions/weatherActions.ts
@@ -2,10 +2,12 @@ import { ThunkAction } from 'redux-thunk';
 import { RootState } from '..';
 import { WeatherAction, WeatherData, WeatherError, GET_WEATHER, SET_LOADING, SET_ERROR } from '../types';
 
+const REACT_APP_API_KEY = '3a1e2067981935a43256c7c43480d51a';
+
 export const getWeather = (city: string): ThunkAction<void, RootState, null, WeatherAction> => {
   return async dispatch => {
     try {
-      const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.REACT_APP_API_KEY}`);
+      const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${REACT_APP_API_KEY}`);
 
       if(!res.ok) {
         const resData: WeatherError = await res.json();
